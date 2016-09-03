dffac0e667a32b6ad301c392bfb535b404f066dd
Toyofumi Fujiwara <sh1@dream.jp>
2016-09-03 14:15:54 +0900

    for windows version, use MeCab

--- convertCspText.py
+++ convertCspText.py
@@ -13,6 +13,41 @@ speak_separator_source_mac = '\n\n'
 speak_separator_converted = "\nl\n"
 
 
+def split_str_mecab(string, return_num=24):
+    'split speak by mecab'
+    import MeCab
+    mecab = MeCab.Tagger("mecabrc")
+    mecab.parse('')  # for MeCab bug
+    encoded = string  # for MeCab bug
+    node = mecab.parseToNode(encoded)
+
+    speakCount = 0
+    speak = ""
+
+    while node:
+        c = node.surface
+        p = node.feature.split(',')[0]
+
+        if speakCount > return_num and (p == '助詞'):
+            speak = speak + c + "\n"
+            speakCount = 0
+        elif speakCount > return_num and (c == '…'):
+            speak = speak + c + "\n"
+            speakCount = 0
+        else:
+            speak = speak + c
+            speakCount += node.length
+        node = node.next
+
+    return speak + "\n"
+
+
+def split_str(s, return_num=10):
+    "speak return by its length 10"
+    length = len(s)
+    return [s[i:i+return_num] for i in range(0, length, return_num)]
+
+
 def splitPage(string, use_mecab=True):
     ary = string.split(row_separator_source)
     reary = [splitLine(v, use_mecab) for v in ary]
@@ -24,8 +59,6 @@ def splitLine(string, use_mecab=True):
     sep = speak_separator_source_mac if os.name is 'posix' else speak_separator_source_win
     ary = string.split(sep)
     reary = [convertCspText(v, use_mecab) for v in ary]
-    #func = convertCspTextByMecab if use_mecab else convertCspText
-    #reary = [func(v) for v in ary]
     return speak_separator_converted.join(reary)
 
 
@@ -56,42 +89,9 @@ def convertCspText(string, use_mecab=True):
             if c == v["pre"]:
                 v["flag"] = True
 
-    def split_str(s, n):
-        "split string by its length"
-        length = len(s)
-        return [s[i:i+n] for i in range(0, length, n)]
-
-
-
-    def split_str_mecab(string, n):
-        import MeCab
-        mecab = MeCab.Tagger("mecabrc")
-        mecab.parse('')  # これを追記！
-
-        encoded = string
-        node = mecab.parseToNode(encoded)
-        speakCount = 0
-        speak = "" 
-        while node:
-            c = node.surface
-            p = node.feature.split(',')[0]
-
-            if speakCount > 24 and (p == '助詞'):
-                speak = speak + c + "\n"
-                speakCount = 0
-            else:
-                speak = speak + c
-                speakCount += node.length
-
-            node = node.next
-
-        #print(speak)
-        return speak + "\n"
-
     if not use_mecab:
-        ary = ["\n".join(split_str(v, 10)) for v in ary]
+        ary = ["\n".join(split_str(v)) for v in ary]
     else:
-        ary = [split_str_mecab(v, 10) for v in ary] 
+        ary = [split_str_mecab(v) for v in ary]
 
     return speakingSeparator.join(ary)
-
